04b7ff12d34f7be2dad8557b0c04ba9fa2f30a8f
Charles2205 <cewudzie0@gmail.com>
2023-08-21 12:22:54 +0000

    fixed bugs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const dotEnv = require('dotenv')
 const userRoute = require('./routes/pagesRoute')
 const ejs = require('ejs')
+const expressLayouts = require('express-ejs-layouts');
+
 
 dotEnv.config()
 const PORT = process.env.PORT || 1209
@@ -10,8 +12,13 @@ const app = express()
 
 app.set('view engine','ejs')
 
-app.use('/',userRoute)
+// adding middlewares
 app.use(express.static('public'))
+app.use(expressLayouts)
+
+
+//user routes
+app.use('/',userRoute)
 
 
 app.listen(PORT,()=>{
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,5 +1,4 @@
-const fs = require('fs')
-const userUrl = require('url')
+
 
 
 const home =(req,res)=>{
